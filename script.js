68a5861f06942d7e5e0d3ac2e990159dd20a0ae3
Shubham Soni <153941338+Soni-Shubham1806@users.noreply.github.com>
2024-04-12 18:07:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ let turnO=true;
 let newGameBtn=document.querySelector("#new-btn");
 let msgContainer=document.querySelector(".msg-container");
 let msg=document.querySelector("#msg");
-let count=0;
 const winPatterns =[[0,1,2],[0,3,6],[0,4,8],[1,4,7],[2,5,8],[2,4,6],[3,4,5],[6,7,8]];
 
 
@@ -18,16 +17,13 @@ boxes.forEach((box)=>{
     box.addEventListener("click" ,()=>{
         if(turnO){
             box.innerText="O";
-            box.classList.add("red");
             turnO=false;
         }else{
             box.innerText="X";
-            box.classList.add("green");
             turnO=true;
         }
         box.disabled=true;
-        count=count+1;
-        checkWinner(count);
+        checkWinner();
     })
 });
 
@@ -48,12 +44,7 @@ const showWinner=(winner)=>{
     disableBoxes();
 };
 
-const showDraw=()=>{
-    msg.innerText="Match Draw";
-    msgContainer.classList.remove("hide");
-    disableBoxes();
-};
-const checkWinner=(count)=>{
+const checkWinner=()=>{
     for(let pattern of winPatterns){
         let pos1Val=boxes[pattern[0]].innerText;
         let pos2Val=boxes[pattern[1]].innerText;
@@ -62,9 +53,6 @@ const checkWinner=(count)=>{
         if(pos1Val!="" && pos2Val!="" && pos3Val!=""){
             if(pos1Val===pos2Val && pos2Val===pos3Val){
                 showWinner(pos1Val);
-            }else{
-                if(count===9)
-                showDraw();
             }
         }
     }
